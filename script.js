1acc009a7496c5e2688151dc5c3c43cadaa0c698
Ítalo Gabriel <99662496+italogabriel1994@users.noreply.github.com>
2023-03-29 09:25:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+// abrir e fechar janela quem somos
+function openNewScreen() {
+    var newScreen = document.getElementById("new-screen");
+    newScreen.classList.remove("hidden");
+    setTimeout(function() {
+      newScreen.classList.add("open");
+    }, 10);
+  }
+  
+  function closeNewScreen() {
+    var newScreen = document.getElementById("new-screen");
+    newScreen.classList.remove("open");
+    setTimeout(function() {
+      newScreen.classList.add("hidden");
+    }, 500);
+  }
+
+  // abrir e fechar janela quem somos
+function openNewScreen() {
+    var newScreen = document.getElementById("new-screen");
+    newScreen.classList.remove("hidden");
+    setTimeout(function() {
+      newScreen.classList.add("open");
+    }, 10);
+  }
+  
+  function closeNewScreen() {
+    var newScreen = document.getElementById("new-screen");
+    newScreen.classList.remove("open");
+    setTimeout(function() {
+      newScreen.classList.add("hidden");
+    }, 500);
+  }
+
+
